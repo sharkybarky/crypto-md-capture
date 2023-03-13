9e40bc7d5cc6d661ed60c87ea3ad2d41a56af20f
Richard Sharrock <richard.sharrock@gmail.com>
2023-03-13 21:03:18 +0000

    moved to using normalised decimal string representation of price as dict key for order book added additional order book state unit tests

--- order_book_state.py
+++ order_book_state.py
@@ -11,30 +11,45 @@ MAX_BOOK_RENDER_DEPTH = 10
 log = getLogger(__name__)
 
 
+class BookLevel:
+    def __init__(self, price, quantity, side):
+        self.price = price
+        self.quantity = quantity
+        self.side = side
+
+    def __lt__(self, other):
+        return self.price < other.price
+
+    def __repr__(self):
+        return f"BookLevel({self.side}: {self.price}@{self.quantity})"
+
+
 class OrderBookState:
     def __init__(self, symbol, render_flag=False, trade_queue=None):
         self.symbol = symbol
         self.book = {'BID': {}, 'ASK': {}}
+        # self.book_by_level_bids: [BookLevel] = []
+        # self.book_by_level_asks: [BookLevel] = []
         self.trade_processor = TradeProcessor(trade_queue)
-        self.order_register = {}
+        self.book_by_order_register = {}
         self.sequence_num = 0
         self.out_of_sequence_restart = False
         self.render_book = render_flag
 
     def on_initial(self, initial_msg: [], side_of_book: str, msg_time: dt.datetime, rec_time: dt.datetime):
         for count, book_details in enumerate(initial_msg):
-            price = float(book_details['price'])
-            volume = Decimal(book_details['volume'])
+            price = str(Decimal(book_details['price']).normalize())
+            volume = Decimal(book_details['volume']).normalize()
             order_id = book_details['id']
             log.debug(f"{count}: {price}@{volume}: {order_id}")
 
             # add to order record (needed so we can look up details when we receive deletes)
-            self.order_register.update({order_id: {'type': side_of_book,
-                                                   'price': price,
-                                                   'volume': volume,
-                                                   'exchange_timestamp': msg_time,
-                                                   'received_timestamp': rec_time}
-                                        })
+            self.book_by_order_register.update({order_id: {'type': side_of_book,
+                                                           'price': Decimal(price).normalize(),
+                                                           'volume': volume,
+                                                           'exchange_timestamp': msg_time,
+                                                           'received_timestamp': rec_time}
+                                                })
             # build book
             volume += self.book[side_of_book].get(price, Decimal(0))  # add this volume to any existing at this level
             self.book[side_of_book].update({price: volume})  # TODO: add id to FILO list?
@@ -49,17 +64,17 @@ class OrderBookState:
 
     def on_trade(self, trade_msg, msg_time: dt.datetime, rec_time: dt.datetime):
         for trade in trade_msg:
-            trade_base = Decimal(trade['base'])
-            trade_counter = Decimal(trade['counter'])
+            trade_base = Decimal(trade['base']).normalize()
+            trade_counter = Decimal(trade['counter']).normalize()
 
             maker_id = trade.get('maker_order_id')
-            maker_order = self.order_register.get(maker_id)
+            maker_order = self.book_by_order_register.get(maker_id)
 
             taker_id = trade.get('taker_order_id')
-            taker_order = self.order_register.get(taker_id)
+            taker_order = self.book_by_order_register.get(taker_id)
 
             order_id = trade.get('order_id')
-            order = self.order_register.get(order_id)
+            order = self.book_by_order_register.get(order_id)
             self.trade_processor.on_trade(self.symbol, order, trade_base, trade_counter, msg_time, rec_time)
 
             log.debug(f"\n{maker_order=}, \n{taker_order=}, \n{order=}")
@@ -70,21 +85,21 @@ class OrderBookState:
                 log.debug(f"Updated maker order: \n{maker_order=}")
                 if maker_order['volume'] == 0:
                     # remove order from order register if its now zero volume
-                    self.order_register.pop(maker_id)
+                    self.book_by_order_register.pop(maker_id)
                 elif maker_order['volume'] < 0:
                     log.error(f"Order volume went negative! \n{maker_order=}")
                     # handle this case if it ever happens. log error to see
-                self.order_register.update(maker_order)
+                self.book_by_order_register.update(maker_order)
                 self.update_book_after_trade_or_deletion(maker_order, trade_base)
 
             if taker_order:
                 # reduce the maker volume by the size of the trade
                 taker_order['volume'] -= trade_base
                 log.debug(f"Updated taker order: \n{taker_order=}")
-                if taker_order['volume'] <= 0:
-                    log.error("Order volume went to zero or negative!")
+                if taker_order['volume'] < 0:
+                    log.error(f"Order volume went negative! \n{taker_order=}")
                     # handle this case if it ever happens. log error to see
-                self.order_register.update(taker_order)
+                self.book_by_order_register.update(taker_order)
                 self.update_book_after_trade_or_deletion(taker_order, trade_base)
 
             if not maker_order and not taker_order:
@@ -94,32 +109,35 @@ class OrderBookState:
 
     def update_book_after_trade_or_deletion(self, order, reduce_by):
         # update book
-        level = self.book[order['type']].get(order['price'])
+        if isinstance(order['price'], Decimal):
+            lookup_price = str(order['price'])
+        else:
+            lookup_price = order['price']
+        level = self.book[order['type']].get(lookup_price)
         if level:
             new_level = level - reduce_by
-            price = order['price']
             if new_level < 0:
                 log.error("Somethings gone wrong, price in book reduced below 0!")
             elif new_level > 0:
-                self.book[order['type']].update({price: new_level})
+                self.book[order['type']].update({lookup_price: new_level})
             else:
                 # remove level entirely
-                removed = self.book[order['type']].pop(price)
+                removed = self.book[order['type']].pop(lookup_price)
                 log.debug(f"Removed entire level: {removed=}")
         else:
             log.error("Somethings gone wrong updating book, trying to update or remove an order which doesn't exist!")
 
-    def on_create(self, create_msg, msg_time: dt.datetime, rec_time: dt.datetime):
+    def on_create(self, create_msg: dict, msg_time: dt.datetime, rec_time: dt.datetime):
         order_id = create_msg.get('order_id')
         side_of_book = create_msg.get('type')
-        price = float(create_msg.get('price'))
-        volume = Decimal(create_msg.get('volume'))
+        price = str(Decimal(create_msg.get('price')).normalize())
+        volume = Decimal(create_msg.get('volume')).normalize()
         created = {order_id: {'type': side_of_book,
-                              'price': price,
+                              'price': Decimal(price),
                               'volume': volume,
                               'exchange_timestamp': msg_time,
                               'received_timestamp': rec_time}}
-        self.order_register.update(created)
+        self.book_by_order_register.update(created)
         # update book
         volume += self.book[side_of_book].get(price, Decimal(0))  # add this volume to any existing at this level
         self.book[side_of_book].update({price: volume})
@@ -129,9 +147,9 @@ class OrderBookState:
 
     def on_delete(self, delete_msg, msg_time: dt.datetime, rec_time: dt.datetime):
         order_id = delete_msg.get('order_id')
-        order = self.order_register.get(order_id)
+        order = self.book_by_order_register.get(order_id)
         if order:
-            removed = self.order_register.pop(order_id)
+            removed = self.book_by_order_register.pop(order_id)
             log.debug(f"Order deleted: \n{removed=}")
             # remove reduce volume from book or remove price entirely
             self.update_book_after_trade_or_deletion(order, reduce_by=order['volume'])
--- tests/test_order_book_state.py
+++ tests/test_order_book_state.py
@@ -4,28 +4,39 @@ from decimal import *
 from order_book_state import OrderBookState
 import datetime as dt
 
-msg_asks_initial_basic = [
+MSG_ASKS_INITIAL_BASIC = [
     {'id': 'ask_order1', 'price': '1.00000000', 'volume': '0.01'},
     {'id': 'ask_order2', 'price': '1.25000000', 'volume': '2.02'}
 ]
 
-msg_bids_initial_basic = [
+MSG_BIDS_INITIAL_BASIC = [
     {'id': 'bid_order1', 'price': '0.80000000', 'volume': '0.01'},
     {'id': 'bid_order2', 'price': '0.70000000', 'volume': '2.02'}
 ]
 
-msg_asks_initial_same_level = [
+MSG_ASKS_INITIAL_SAME_LEVEL = [
     {'id': 'ask_order1', 'price': '2.00000000', 'volume': '1.01'},
     {'id': 'ask_order2', 'price': '2.00000000', 'volume': '2.02'}
 ]
 
-msg_bids_initial_same_level = [
+MSG_BIDS_INITIAL_SAME_LEVEL = [
     {'id': 'bid_order1', 'price': '1.00000000', 'volume': '1.01'},
     {'id': 'bid_order2', 'price': '1.00000000', 'volume': '2.02'}
 ]
 
-msg_create_ask = {'order_id': 'BXNR56AAFSFJM5G', 'type': 'ASK', 'price': '442091.00000000', 'volume': '0.01500000'}
-msg_create_bid = {'order_id': 'BXBQ7Z82V2PK5ZH', 'type': 'BID', 'price': '441055.00000000', 'volume': '0.01350000'}
+MSG_CREATE_BID = {'order_id': 'BXBQ7Z82V2PK5ZH', 'type': 'BID', 'price': '0.90000000', 'volume': '1.50'}
+MSG_CREATE_ASK = {'order_id': 'BXNR56AAFSFJM5G', 'type': 'ASK', 'price': '0.95000000', 'volume': '1.50'}
+MSG_CREATE_ASK2 = {'order_id': 'BXNR56AAFSFJM5G', 'type': 'ASK', 'price': '0.1', 'volume': '0.01'}
+MSG_CREATE_ASK3 = {'order_id': 'BXNR56AAFSFJM5G', 'type': 'ASK', 'price': '1.0000000', 'volume': '0.01'}
+MSG_CREATE_BID2 = {'order_id': 'BXNR56AAFSFJM5G', 'type': 'BID', 'price': '0.80000000', 'volume': '0.01'}
+
+MSG_DELETE = {'order_id': 'ask_order1'}
+MSG_DELETE2 = {'order_id': 'BXNR56AAFSFJM5G'}
+MSG_DELETE_INVALID = {'order_id': 'no_record_of_this_order_id'}
+
+MSG_TRADE_SOME = [{'base': '0.00112000', 'counter': '499.8201600000000000', 'maker_order_id': 'ask_order1', 'taker_order_id': 'BXDSJ2B8VQHU7VG', 'order_id': 'ask_order1'}]
+MSG_TRADE_ALL_TAKER = [{'base': '0.01', 'counter': '104.440000', 'maker_order_id': 'BXDSJ2B8VQHU7VG', 'taker_order_id': 'bid_order1', 'order_id': 'bid_order1'}]
+MSG_TRADE_ALL_MAKER = [{'base': '0.01', 'counter': '104.440000', 'maker_order_id': 'bid_order1', 'taker_order_id': 'BXDSJ2B8VQHU7VG', 'order_id': 'bid_order1'}]
 
 
 class OrderBookStateTest(unittest.TestCase):
@@ -43,46 +54,96 @@ class OrderBookStateTest(unittest.TestCase):
         return book
 
     def test_initial_basic(self):
-        book_state = self.construct_book(msg_bids_initial_basic, msg_asks_initial_basic)
+        book_state = self.construct_book(MSG_BIDS_INITIAL_BASIC, MSG_ASKS_INITIAL_BASIC)
 
         # validate we get the orders from the register okay
-        order1 = book_state.order_register.get('ask_order1')
-        order2 = book_state.order_register.get('ask_order2')
+        order1 = book_state.book_by_order_register.get('ask_order1')
+        order2 = book_state.book_by_order_register.get('ask_order2')
         self.assertIsNotNone(order1)
         self.assertIsNotNone(order2)
 
         # validate the book_state looks okay
         bid_side = book_state.book.get('BID')
         ask_side = book_state.book.get('ASK')
-        self.assertEqual(bid_side, {0.8: Decimal('0.01'), 0.7: Decimal('2.02')})
-        self.assertEqual(ask_side, {1.0: Decimal('0.01'), 1.25: Decimal('2.02')})
+        self.assertEqual({'0.8': Decimal('0.01'), '0.7': Decimal('2.02')}, bid_side)
+        self.assertEqual({'1': Decimal('0.01'), '1.25': Decimal('2.02')}, ask_side)
 
     def test_initial_price_levels_summed(self):
-        book_state = self.construct_book(msg_bids_initial_same_level, msg_asks_initial_same_level)
+        book_state = self.construct_book(MSG_BIDS_INITIAL_SAME_LEVEL, MSG_ASKS_INITIAL_SAME_LEVEL)
         bid_side = book_state.book.get('BID')
         ask_side = book_state.book.get('ASK')
-        self.assertEqual(bid_side, {1.0: Decimal('3.03')})
-        self.assertEqual(ask_side, {2.0: Decimal('3.03')})
+        self.assertEqual({'1': Decimal('3.03')}, bid_side)
+        self.assertEqual({'2': Decimal('3.03')}, ask_side)
 
     def test_order_book_on_create_msg(self):
-        book_state = self.construct_book(msg_bids_initial_basic, msg_asks_initial_basic)
-        # book_state.on_create()
+        book_state = self.construct_book(MSG_BIDS_INITIAL_BASIC, MSG_ASKS_INITIAL_BASIC)
+        book_state.on_create(create_msg=MSG_CREATE_ASK, msg_time=self.msg_time, rec_time=self.tnow)
+        book_state.on_create(create_msg=MSG_CREATE_BID, msg_time=self.msg_time, rec_time=self.tnow)
+        book_state.on_create(create_msg=MSG_CREATE_ASK2, msg_time=self.msg_time, rec_time=self.tnow)
+        book_state.on_create(create_msg=MSG_CREATE_ASK3, msg_time=self.msg_time, rec_time=self.tnow)
+        book_state.on_create(create_msg=MSG_CREATE_BID2, msg_time=self.msg_time, rec_time=self.tnow)
+        bid_side = book_state.book.get('BID')
+        self.assertEqual(Decimal('0.01') + Decimal('0.01'), bid_side['0.8'])
+        self.assertEqual(Decimal('2.02'), bid_side['0.7'])
+        self.assertEqual(Decimal('1.50'), bid_side['0.9'])
 
+        ask_side = book_state.book.get('ASK')
+        self.assertEqual(Decimal('1.5'), ask_side['0.95'])
+        self.assertEqual(Decimal('0.01'), ask_side['0.1'])
+        self.assertEqual(Decimal('0.01') + Decimal('0.01'), ask_side['1'])
+        self.assertEqual(Decimal('2.02'), ask_side['1.25'])
 
     def test_order_book_on_delete_msg(self):
-        pass
+        book_state = self.construct_book(MSG_BIDS_INITIAL_BASIC, MSG_ASKS_INITIAL_BASIC)
+        book_state.on_delete(delete_msg=MSG_DELETE, msg_time=self.msg_time, rec_time=self.tnow)
+        bid_side = book_state.book.get('BID')
+        ask_side = book_state.book.get('ASK')
+        self.assertEqual({'0.8': Decimal('0.01'), '0.7': Decimal('2.02')}, bid_side)
+        self.assertEqual({'1.25': Decimal('2.02')}, ask_side)
+        book_state.on_create(create_msg=MSG_CREATE_BID2, msg_time=self.msg_time, rec_time=self.tnow)
+        bid_side = book_state.book.get('BID')
+        ask_side = book_state.book.get('ASK')
+        self.assertEqual({'0.8': Decimal('0.02'), '0.7': Decimal('2.02')}, bid_side)
+        self.assertEqual({'1.25': Decimal('2.02')}, ask_side)
+        book_state.on_delete(delete_msg=MSG_DELETE2, msg_time=self.msg_time, rec_time=self.tnow)
+        bid_side = book_state.book.get('BID')
+        ask_side = book_state.book.get('ASK')
+        self.assertEqual({'0.8': Decimal('0.01'), '0.7': Decimal('2.02')}, bid_side)
+        self.assertEqual({'1.25': Decimal('2.02')}, ask_side)
 
     def test_order_book_on_delete_thats_not_there(self):
-        pass
+        book_state = self.construct_book(MSG_BIDS_INITIAL_BASIC, MSG_ASKS_INITIAL_BASIC)
+
+        with self.assertLogs(logger='order_book_state', level='WARNING') as log:
+            book_state.on_delete(delete_msg=MSG_DELETE_INVALID, msg_time=self.msg_time, rec_time=self.tnow)
+            self.assertIn('WARNING:order_book_state:Received a delete for an order which we have no record!',
+                          log.output)
 
     def test_order_book_on_trade_occurred(self):
-        pass
+        book_state = self.construct_book(MSG_BIDS_INITIAL_BASIC, MSG_ASKS_INITIAL_BASIC)
+        book_state.on_trade(trade_msg=MSG_TRADE_SOME, msg_time=self.msg_time, rec_time=self.tnow)
+        bid_side = book_state.book.get('BID')
+        ask_side = book_state.book.get('ASK')
+        self.assertEqual({'0.8': Decimal('0.01'), '0.7': Decimal('2.02')}, bid_side)
+        self.assertEqual({'1': Decimal('0.01') - Decimal('0.00112'), '1.25': Decimal('2.02')}, ask_side)
 
     def test_order_book_on_trade_occurred_resulting_in_zero_volume_at_level(self):
-        pass
+        book_state = self.construct_book(MSG_BIDS_INITIAL_BASIC, MSG_ASKS_INITIAL_BASIC)
+        book_state.on_trade(trade_msg=MSG_TRADE_ALL_TAKER, msg_time=self.msg_time, rec_time=self.tnow)
+        bid_side = book_state.book.get('BID')
+        ask_side = book_state.book.get('ASK')
+        self.assertEqual({'0.7': Decimal('2.02')}, bid_side)
+        self.assertEqual({'1': Decimal('0.01'), '1.25': Decimal('2.02')}, ask_side)
 
-    def test_initial_corrupt_calls(self):
-        pass
+        book_state = self.construct_book(MSG_BIDS_INITIAL_BASIC, MSG_ASKS_INITIAL_BASIC)
+        book_state.on_trade(trade_msg=MSG_TRADE_ALL_MAKER, msg_time=self.msg_time, rec_time=self.tnow)
+        bid_side = book_state.book.get('BID')
+        ask_side = book_state.book.get('ASK')
+        self.assertEqual({'0.7': Decimal('2.02')}, bid_side)
+        self.assertEqual({'1': Decimal('0.01'), '1.25': Decimal('2.02')}, ask_side)
+
+    # def test_initial_msg_when_corrupt(self):
+    #     pass
 
 
 if __name__ == '__main__':
